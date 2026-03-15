ed07c7d025ca1d7c4b736490cabf5bd4130ab645
Ceejay Santos <7078ceejay@gmail.com>
2026-03-15 20:09:48 +0800

    setup

---
+++ backend/api/user/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from datetime import timedelta
+
+# class Test(models.Model):
+#     name = models.CharField(max_length=30, unique=True)
+#     description = models.TextField(default="set a description")
+#     created_at = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return self.name
+
+class Profile(models.Model):
+
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('provider', 'Service Provider'),
+        ('customer', 'Customer'),
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile",)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='customer')
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    profile_image = models.ImageField(upload_to="profiles/", blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+    
---
+++ backend/api/user/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Profile
+
+class ProfileSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Profile
+        fields = [
+            "role",
+            "phone",
+            "profile_image",
+        ]
+        read_only_fields = ["id"]
+
+class UserSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer(read_only=True)
+    
+    class Meta:
+       model = User
+       fields = ('id', 'username', 'email', 'password', "profile")
+       extra_kwargs = {'password': {'write_only': True}}
+       
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            
+        )
+        return user
+    
