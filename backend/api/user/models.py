6e908f7f877db4322b82d46ee9fee2c92cc9301c
Ceejay Santos <7078ceejay@gmail.com>
2026-03-20 13:13:12 +0800

    location rquired

--- backend/api/user/models.py
+++ backend/api/user/models.py
@@ -33,8 +33,9 @@ class Profile(models.Model):
 class Location(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="saved_location",)
     name = models.CharField(max_length=200)
-    latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
-    longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
+    address = models.CharField(max_length=200)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
     
     def __str__(self):
         return self.name
