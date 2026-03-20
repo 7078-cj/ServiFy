d09a4190286585a50a0fa85fc8caf592d8de7bf7
Ceejay Santos <7078ceejay@gmail.com>
2026-03-20 10:20:31 +0800

    from having only one location per user to one to many

--- backend/api/user/models.py
+++ backend/api/user/models.py
@@ -28,7 +28,7 @@ class Profile(models.Model):
         return self.user.username
     
 class Location(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="saved_location",)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="saved_location",)
     name = models.CharField(max_length=200)
     latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
     longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
