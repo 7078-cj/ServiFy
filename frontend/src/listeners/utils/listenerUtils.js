15430987135aeed7cd2c7898e190b919abcd1892
Ceejay Santos <7078ceejay@gmail.com>
2026-04-09 02:44:34 +0800

    feat: refactor handleMessage function to destructure message and improve event handling

--- frontend/src/listeners/utils/listenerUtils.js
+++ frontend/src/listeners/utils/listenerUtils.js
@@ -1,12 +1,18 @@
-export function handleMessage(data,set) {
-    console.log("Received booking event:", data)
-    if (data.type === "created") {
+export function handleMessage(message, set) {
+
+    const { type, data } = message
+
+    if (type === "created") {
         set((prev) => [...prev, data])
-    } else if (data.type === "updated") {
+
+    } else if (type === "updated") {
         set((prev) =>
             prev.map((b) => (b.id === data.id ? data : b))
         )
-    } else if (data.type === "deleted") {
-        set((prev) => prev.filter((b) => b.id !== data))
+
+    } else if (type === "deleted") {
+        set((prev) =>
+            prev.filter((b) => b.id !== data)
+        )
     }
 }
