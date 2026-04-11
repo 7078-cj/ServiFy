3798f6e13da84cfa0c8d65c1ffaed75868a82441
Ceejay Santos <7078ceejay@gmail.com>
2026-04-12 02:54:54 +0800

    fix added api in makemigrations

--- backend/executables/start_web.sh
+++ backend/executables/start_web.sh
@@ -2,7 +2,7 @@
 
 # Start Django server
 python manage.py migrate
-python manage.py makemigrations
+python manage.py makemigrations api
 python manage.py migrate api
 python manage.py migrate django_celery_beat
 python manage.py runserver 0.0.0.0:8000
